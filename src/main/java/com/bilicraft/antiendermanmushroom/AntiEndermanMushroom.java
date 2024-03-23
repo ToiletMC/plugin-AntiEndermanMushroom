d689a867cc72bad68d7e0688ac2d92d83a71e8fc
TheLittle_Yang <2570984321@qq.com>
2024-03-23 22:06:33 +0800

    由于服务器的主世界使用了特别的地形生成器，有些地方蘑菇很多。所以必须阻止主世界的末影人拿起蘑菇。

--- src/main/java/com/bilicraft/antiendermanmushroom/AntiEndermanMushroom.java
+++ src/main/java/com/bilicraft/antiendermanmushroom/AntiEndermanMushroom.java
@@ -2,7 +2,6 @@ package com.bilicraft.antiendermanmushroom;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.World;
 import org.bukkit.entity.Enderman;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -15,29 +14,27 @@ public final class AntiEndermanMushroom extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         // Plugin startup logic
-        Bukkit.getPluginManager().registerEvents(this,this);
+        Bukkit.getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
+
     @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
-    public void onEndermanPutMushroom(EntityChangeBlockEvent event){
-        if(!(event.getEntity() instanceof Enderman)){
+    public void onEndermanPutMushroom(EntityChangeBlockEvent event) {
+        if (!(event.getEntity() instanceof Enderman)) {
             return;
         }
-        if(event.getEntity().getWorld().getEnvironment() == World.Environment.NORMAL){
-            return;
-        }
-        if(isMushroom(event.getTo()) || isMushroom(event.getBlockData().getMaterial())){
+        if (isMushroom(event.getTo()) || isMushroom(event.getBlockData().getMaterial())) {
             event.setCancelled(true);
             Enderman enderman = (Enderman) event.getEntity();
             enderman.setCarriedBlock(null);
         }
     }
 
-    private boolean isMushroom(Material type){
+    private boolean isMushroom(Material type) {
         return type == Material.BROWN_MUSHROOM
                 || type == Material.RED_MUSHROOM
                 || type == Material.CRIMSON_FUNGUS
